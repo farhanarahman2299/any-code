f9342aa020ccf5f93684bfca7022f934815f1096
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2019-12-01 01:39:15 +0600

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,11 @@
+
+package PERSON;
+
+public class Class {
+
+    public static void main(String[] args) {
+    System.out.println("Starting Main Method");
+    System.out.println("who am i!");
+    
+    
+  }
